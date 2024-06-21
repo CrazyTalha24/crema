d3c6a1f2e49abdcb988555e5e0a9c3bd05cc4bec
Talha Islam <86923018+CrazyTalha@users.noreply.github.com>
2024-06-21 19:34:10 +0500

    Added Javascript for toggle navbar, made navbar responsive, and added sliding down animation for navbar. Completed the webpage!

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 $(document).ready(function () {
 
 
-    $('.js--section--features').waypoint(function(direction) {
+    /* For Sticky Nav */
+
+    $('.js--section--features').waypoint(function (direction) {
         if (direction == "down") {
-            $('nav').addClass('sticky');
+            $('nav').addClass('sticky animate__animated animate__slideInDown animate__faster');
         } else {
-            $('nav').removeClass('sticky');
+            $('nav').addClass('animate__animated animate__slideOutUp').removeClass('animate__slideInDown');
+
+            setTimeout(function () {
+                $('nav').removeClass('sticky animate__slideOutUp');
+            }, 100);
         }
+    }, {
+        offset: '60px;'
     });
 
-    /*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-        notify(this.element.id + ' hit 25% from top of window') 
-      }, {
-        offset: '25%'
-      })
-    */
+
+
+    /* Scroll on Button */
+
+    $('.js--scroll-to-book').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-book').offset().top }, 100);
+    });
+
+
+    /* Mobile navigation */
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon');
+        
+        if (icon.attr('name') == 'menu-outline') {
+            icon.attr('name', 'close-outline');
+        }
+        else {
+            icon.attr('name', 'menu-outline')
+        }
+
+        nav.slideToggle(200);
+    });
+
 });
+
+// let navbar = document.getElementById("navigation");
+
+// let shouldStickPosition = navbar.offsetHeight;
+
+// function addOrRemoveStickyClass() {
+//     if (window.scrollY >= shouldStickPosition) {
+//         navbar.classList.add("sticky");
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+// window.onscroll = () => {
+//     addOrRemoveStickyClass();
+// };
+
+// window.onresize = () => {
+//     shouldStickPosition = navbar.offsetTop;
+// };
